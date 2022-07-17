4f372fc3f1ead408a79f8a101ed76957e571e818
murallaexe <joksanhaniel@gmail.com>
2022-07-17 00:01:48 -0600

    Subiendo cambio2

--- app/src/main/java/pps/realty/myapplication2/MainActivity.java
+++ app/src/main/java/pps/realty/myapplication2/MainActivity.java
@@ -16,13 +16,12 @@ import pps.realty.myapplication2.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivityMainBinding.inflate(getLayoutInflater());
+        pps.realty.myapplication2.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
      setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
